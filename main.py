49a657d76f80365d41c417bfd937aaecc9315a23
serban-hartular <hartular@gmail.com>
2021-08-14 22:50:08 +0300

    Added static site

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, send_from_directory
-import random
 from flask import request
 import json
 
@@ -8,33 +7,37 @@ from process_request import text_to_treelist
 app = Flask(__name__)
 
 # Path for our main Svelte page
-@app.route("/old")
-def base():
-    return send_from_directory('client-old/public', 'index.html')
-
 @app.route("/")
 def test():
     return send_from_directory('client/public', 'index.html')
-
 
 # Path for all the static files (compiled JS/CSS, etc.)
 @app.route("/<path:path>")
 def home(path):
     return send_from_directory('client/public', path)
 
-@app.route("/rand")
-def hello():
-    n = random.randint(0, 100)
-    print(n)
-    return str(n)
+@app.route("/static/")
+def static_dir_index():
+    return send_from_directory("static", "index.html")
 
-@app.route("/echo", methods=['POST'])
-def echo():
-    obj = request.get_json()
-    print(obj)
-    obj['text'] = obj['text'] * 2
-    print(json.dumps(obj))
-    return json.dumps(obj)
+
+@app.route("/static/<path:path>")
+def static_dir(path):
+    return send_from_directory("static", path)
+
+# @app.route("/rand")
+# def hello():
+#     n = random.randint(0, 100)
+#     print(n)
+#     return str(n)
+# 
+# @app.route("/echo", methods=['POST'])
+# def echo():
+#     obj = request.get_json()
+#     print(obj)
+#     obj['text'] = obj['text'] * 2
+#     print(json.dumps(obj))
+#     return json.dumps(obj)
 
 @app.route("/parse", methods=['POST'])
 def parse_text():
@@ -49,7 +52,7 @@ def parse_text():
         text = obj['text']
         lang = obj['lang']
     except Exception as e:
-        return_obj['error_msg'] = str(e)
+        return_obj['error_msg'] = 'Error: Request lacks property ' + str(e)
         return return_obj
     parse_obj = text_to_treelist(text, lang)
     return_obj['tree'] = parse_obj
